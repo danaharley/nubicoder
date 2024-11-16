f5655bb4ea2e4e861d5377193c4d1f8857926663
danaharley <30239616+danaharley@users.noreply.github.com>
2024-11-17 01:00:10 +0700

    fix ThemeProviderProps

--- providers/theme-provider.tsx
+++ providers/theme-provider.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { ThemeProviderProps } from "next-themes/dist/types";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import {
+  ThemeProvider as NextThemesProvider,
+  ThemeProviderProps,
+} from "next-themes";
 
 export const ThemeProvider = ({ children, ...rest }: ThemeProviderProps) => {
   return <NextThemesProvider {...rest}>{children}</NextThemesProvider>;
